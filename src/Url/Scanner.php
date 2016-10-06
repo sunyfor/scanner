daf272c8709f647f78185403e11192d8543fab19
sunyfor <sunyfor@naver.com>
2016-10-06 13:56:40 +0900

    002

--- src/Url/Scanner.php
+++ src/Url/Scanner.php
@@ -1,35 +1,39 @@
 <?
-namespace Hanbit\ModernPHP\Url;
+namespace Sunyfor\Scanner\Url;
 
-class Scanner{
-
+class Scanner
+{
     protected $urls;
+    protected $httpClient;
 
-    protected $httpClinet;
-
-    public function __construct(array $urls){
+    public function __construct(array $urls)
+    {
         $this->urls = $urls;
-        $this->$httpClinet = new \GuzzleHttp\Client();
+        $this->httpClient = new \GuzzleHttp\Client();
     }
 
-    public function getInvalidUrls(){
+    public function getInvalidUrls()
+    {
         $invalidUrls = [];
         foreach ($this->urls as $url) {
-            try{
+            try {
                 $statusCode = $this->getStatusCodeForUrl($url);
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 $statusCode = 500;
             }
-            if($statusCode >= 400){
-                array_push($invalidUrls, ['url'=>$url, 'status'=>$statusCode]);
+            if ($statusCode >= 400) {
+                array_push($invalidUrls, [
+                    'url' => $url,
+                    'status' => $statusCode
+                ]);
             }
         }
-
         return $invalidUrls;
     }
 
-    protected function getStatusCodeForUrl($url){
-        $HttpResponse = $this->httpClient->options($url);
-        return $HttpResponse->getStatusCode();
+    protected function getStatusCodeForUrl($url)
+    {
+        $httpResponse = $this->httpClient->options($url);
+        return $httpResponse->getStatusCode();
     }
 }
